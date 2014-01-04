297273b5598d0efada06a6535ab1aeeac6471036
gaxit <gaxit@interia.eu>
2014-01-04 19:22:29 +0100

    Dodanie listowania i usuwania użytkowników

---
+++ src/main/java/pl/rea/client/bean/UserManagementBean.java
@@ -0,0 +1,46 @@
+package pl.rea.client.bean;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.model.DefaultStreamedContent;
+
+import pl.rea.client.service.UserServices;
+import pl.rea.client.webmethods.users.UserCanonical;
+
+@SessionScoped
+@ManagedBean(name="userManagementBean")
+public class UserManagementBean {
+	
+	@ManagedProperty(value="#{loginBean}")
+	private LoginBean loginBean;
+	
+	//----- metody wykonujace akcje -----
+	
+	public List<UserCanonical> getUserList(){
+		UserServices service = new UserServices();
+		return service.getUserList(loginBean.getLogin(), loginBean.getSessionId());
+	}
+	
+	public void deleteUser(){
+		UserServices service = new UserServices();
+		FacesContext context = FacesContext.getCurrentInstance();
+		String login = context.getExternalContext().getRequestParameterMap().get("userLogin");
+		System.out.println("Usunięto?: " + service.deleteUser(loginBean.getLogin(), loginBean.getSessionId(), login));
+	}
+	
+	//----- settery i gettery -----
+
+	public LoginBean getLoginBean() {
+		return loginBean;
+	}
+
+	public void setLoginBean(LoginBean loginBean) {
+		this.loginBean = loginBean;
+	}
+
+}
